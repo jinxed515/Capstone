71979f86af699db65ce5e262700d5b8088adb064
Meghna <meganraj251@gmail.com>
2023-04-26 16:43:54 +0530

    add a card for google map

--- app/screens/NavigationScreen.tsx
+++ app/screens/NavigationScreen.tsx
@@ -1,21 +1,29 @@
 import React, { FC } from "react"
 import { TextStyle, ViewStyle } from "react-native"
-import { Screen, Text } from "../components"
+import { Card, Screen, Text, TextField } from "../components"
 import { DemoTabScreenProps } from "../navigators/DemoNavigator"
 import { spacing } from "../theme"
 import { openLinkInBrowser } from "../utils/openLinkInBrowser"
 import { isRTL } from "../i18n"
 
-export const NavigationScreen: FC<DemoTabScreenProps<"Navigate">> =
-  function NavigationScreen(_props) {
-    return (
-      <Screen preset="scroll" contentContainerStyle={$container} safeAreaEdges={["top"]}>
-        <Text preset="heading" tx="NavigationScreen.title" style={$title} />
-        <Text tx="NavigationScreen.tagLine" style={$tagline} />
-
-      </Screen>
-    )
-  }
+export const NavigationScreen: FC<DemoTabScreenProps<"Navigate">> = function NavigationScreen(
+  _props,
+) {
+  return (
+    <Screen preset="scroll" contentContainerStyle={$container} safeAreaEdges={["top"]}>
+      <Text preset="heading" tx="NavigationScreen.title" style={$title} />
+      <Text tx="NavigationScreen.tagLine" style={$tagline} />
+      <Card
+        heading="The google-maps map"
+        content="This should show the layout"
+        style={{ minHeight: 360 }}
+      />
+      <TextField label="Source Address" value="MG Road" placeholder="Text goes here" />
+      <TextField label="Destination address" value="Home" placeholder="Text goes here" />
+    
+    </Screen>
+  )
+}
 
 const $container: ViewStyle = {
   paddingTop: spacing.large + spacing.extraLarge,
