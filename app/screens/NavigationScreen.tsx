5f7fd25449975f297909d7ced1fe8ea1ee19db0e
nidhih <nidhihemanth123@gmail.com>
2023-05-11 23:51:21 +0530

    submit button + markers for static source & destination

--- app/screens/NavigationScreen.tsx
+++ app/screens/NavigationScreen.tsx
@@ -1,11 +1,11 @@
-import React, { FC, useState, useEffect } from "react"
-import { TextStyle, ViewStyle, StyleSheet, View, Dimensions, Button, TextInput, KeyboardAvoidingView } from "react-native"
+import React, { FC, useState } from "react"
+import { TextStyle, ViewStyle, StyleSheet, View, Dimensions, Button, TextInput, KeyboardAvoidingView, Platform } from "react-native"
 import { Card, Screen, Text, TextField } from "../components"
 import { DemoTabScreenProps } from "../navigators/DemoNavigator"
 import { spacing } from "../theme"
 import { openLinkInBrowser } from "../utils/openLinkInBrowser"
 import { isRTL } from "../i18n"
-import MapView from 'react-native-maps';
+import MapView, { Callout, Marker } from 'react-native-maps';
 
 export const NavigationScreen: FC<DemoTabScreenProps<"Navigate">> = function NavigationScreen(
   _props,
@@ -17,34 +17,69 @@ export const NavigationScreen: FC<DemoTabScreenProps<"Navigate">> = function Nav
     longitudeDelta: 0.009,
   });
 
-  const [source, onChangeSource] = React.useState('');
-  const [destination, onChangeDestination] = React.useState('');
+  const [source, setSource] = React.useState<string>('');
+  const [destination, setDestination] = React.useState<string>('');
+
+  const [marker, setMarker] = React.useState<boolean>(false);
+
+  const handleSubmit = () => {
+    if (source.length == 0) { 
+      console.log("Source Address REQUIRED.") 
+    } else if (destination.length == 0) { 
+      console.log("Destination Address REQUIRED.") 
+    } else {
+      console.log("SOURCE      : ", source);
+      console.log("DESTINATION : ", destination);
+      setMarker(true);
+    }
+  }
 
   return (
     <Screen preset="scroll" contentContainerStyle={$container} style={styles.container} safeAreaEdges={["top"]}>
       <Text preset="heading" tx="NavigationScreen.title" style={$title} />
       <Text tx="NavigationScreen.tagLine" style={$tagline} />
-      <MapView 
-        style={styles.map} 
-        region={mapRegion}
-      >
-        
-      </MapView>
-      <Text tx="NavigationScreen.sourceInput" style={$inputTitle} />
+      {
+        marker 
+        ? 
+        <MapView 
+            style={styles.map} 
+            region={mapRegion}
+          >
+            <Marker coordinate={{latitude: 12.840711, longitude: 77.676369}}>
+              {/* <Callout> <Text>{source}</Text> </Callout> */}
+            </Marker>
+            <Marker coordinate={{latitude: 12.84871, longitude: 77.657882}}>
+              {/* <Callout> <Text>{destination}</Text> </Callout> */}
+            </Marker>
+          </MapView>
+        :
+          <MapView style={styles.map} region={mapRegion}/> 
+      }
       <KeyboardAvoidingView>
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeSource}
-          value={source}
-          placeholder="Enter Starting point"
+        <View>
+          <Text tx="NavigationScreen.sourceInput" style={$inputTitle} />
+          <TextInput
+            style={styles.input}
+            onChangeText={setSource}
+            value={source}
+            placeholder="Enter Starting point"
+          />
+        </View>
+        <View>
+          <Text tx="NavigationScreen.destInput" style={$inputTitle} />
+          <TextInput
+            style={styles.input}
+            onChangeText={setDestination}
+            value={destination}
+            placeholder="Enter Ending point"
+          />
+        </View>
+        <Button
+          onPress={handleSubmit}
+          title="Submit"
+          color="#000"
+          accessibilityLabel="Submit Source & Destination addresses"
         />
-      <Text tx="NavigationScreen.destInput" style={$inputTitle} />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeDestination}
-        value={destination}
-        placeholder="Enter Ending point"
-      />
       </KeyboardAvoidingView>
     </Screen>
   )
@@ -64,8 +99,8 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 10,
     padding: 10,
-    marginBottom: spacing.small,
-  }
+    marginBottom: spacing.medium,
+  },
 });
 
 const $container: ViewStyle = {
@@ -84,6 +119,6 @@ const $tagline: TextStyle = {
 }
 
 const $inputTitle: TextStyle = {
-  marginVertical: "1%",  
+  marginBottom: "1%",  
   fontWeight: "bold",
 }
